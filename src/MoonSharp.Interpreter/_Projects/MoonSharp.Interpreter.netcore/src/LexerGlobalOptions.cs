8822e0459b041a0d239e758ea14042c3d3b04ffe
小仙女 <me@ilyfairy.com>
2024-01-11 13:37:49 +0800

    added Controllable KopiLua_StringLib.MAXCCALLS

--- src/MoonSharp.Interpreter/_Projects/MoonSharp.Interpreter.netcore/src/LexerGlobalOptions.cs
+++ src/MoonSharp.Interpreter/_Projects/MoonSharp.Interpreter.netcore/src/LexerGlobalOptions.cs
@@ -1,10 +1,14 @@
-﻿namespace MoonSharp.Interpreter;
+﻿using MoonSharp.Interpreter.CoreLib.StringLib;
+
+namespace MoonSharp.Interpreter;
 
 public static class LexerGlobalOptions
 {
     public static InvalidEscapeHandling IgnoreInvalid  { get; set; }
 
     public static UnexpectedSymbolHandling UnexpectedSymbolHandling { get; set; }
+
+    public static int PatternMaxCalls { get => KopiLua_StringLib.MAXCCALLS; set => KopiLua_StringLib.MAXCCALLS = value; }
 }
 
 public enum InvalidEscapeHandling
